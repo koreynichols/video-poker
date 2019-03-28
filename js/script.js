4bbb80b269716aaa3cf8b30112639e07386a4653
Korey Nichols <knichols116@hotmail.com>
2019-03-27 20:15:08 -0700

    working on holding cards and drawing cards

--- js/script.js
+++ js/script.js
@@ -4,10 +4,7 @@ let cardImages = ['AS.jpg', '2S.jpg', '3S.jpg', '4S.jpg', '5S.jpg', '6S.jpg', '7
                   'AD.jpg', '2D.jpg', '3D.jpg', '4D.jpg', '5D.jpg', '6D.jpg', '7D.jpg', '8D.jpg', '9D.jpg', '10D.jpg', 'JD.jpg', 'QD.jpg', 'KD.jpg',
                   'AH.jpg', '2H.jpg', '3H.jpg', '4H.jpg', '5H.jpg', '6H.jpg', '7H.jpg', '8H.jpg', '9H.jpg', '10H.jpg', 'JH.jpg', 'QH.jpg', 'KH.jpg'];
 let cardsDelt = [];
-let hand = [];
-let value = [];
-let suit = [];
-let cardsToHold = [false, false, false, false, false];
+let cardsToHold = [];
 
 let dealBtn = document.querySelector('input.deal');
 let image = document.querySelectorAll('img.card');
@@ -22,9 +19,6 @@ image.forEach(function(e){
 
 function dealCards() {
     cardsDelt = [];
-    value = [];
-    suit = [];
-    cardsToHold = [false, false, false, false, false];
 
     while(cardsDelt.length < 5){
         let newCard = Math.floor(Math.random()*52);
@@ -39,11 +33,26 @@ function dealCards() {
 }
 
 function drawCards() {
-    
+    console.log(cardsDelt.length + cardsToHold.length);
     dealBtn.removeAttribute('disabled');
     drawBtn.setAttribute('disabled', true);
 }
 
 function holdCard(e) {
-    console.log(e);
+    if(e.target.classList.contains('hold')){
+        e.target.classList.remove('hold');
+        cardsToHold = arrayRemove(cardsToHold, e.target);
+    } else {
+        e.target.classList.add('hold');
+        cardsToHold.push(e.target);
+    }
+    console.log(e.target);
+    console.log(cardsToHold);
 }
+
+function arrayRemove(arr, value) {
+    return arr.filter(function(ele){
+        return ele != value;
+    });
+ 
+ }
