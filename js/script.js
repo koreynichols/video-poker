0eb131d2aed016d7898824c7ab707883b572622a
Korey Nichols <knichols116@hotmail.com>
2019-04-01 20:04:09 -0700

    working on drawCard function

--- js/script.js
+++ js/script.js
@@ -5,54 +5,87 @@ let cardImages = ['AS.jpg', '2S.jpg', '3S.jpg', '4S.jpg', '5S.jpg', '6S.jpg', '7
                   'AH.jpg', '2H.jpg', '3H.jpg', '4H.jpg', '5H.jpg', '6H.jpg', '7H.jpg', '8H.jpg', '9H.jpg', '10H.jpg', 'JH.jpg', 'QH.jpg', 'KH.jpg'];
 let cardsDelt = [];
 let cardsToHold = [];
+let hand = [];
+
+let image = document.querySelectorAll('img.card');
 
 let dealBtn = document.querySelector('input.deal');
-let image = document.querySelectorAll('img.card');
 let drawBtn = document.querySelector('input.draw');
 
-dealBtn.addEventListener('click', dealCards);
-drawBtn.addEventListener('click', drawCards);
+dealBtn.addEventListener('click', deal);
+drawBtn.addEventListener('click', draw);
 
 image.forEach(function(e){
     e.addEventListener('click', holdCard);
 })
 
-function dealCards() {
+function deal() {
     cardsDelt = [];
+    cardsToHold = [];
+    image.forEach(function(e){
+        e.classList.remove('hold');
+    });
+    dealBtn.setAttribute('disabled', true);
+    drawBtn.removeAttribute('disabled');
+    dealCards();
+}
 
+function draw() {
+    dealBtn.removeAttribute('disabled');
+    drawBtn.setAttribute('disabled', true);
+    drawCards();
+    scoreHand();
+}
+
+function dealCards() {
     while(cardsDelt.length < 5){
         let newCard = Math.floor(Math.random()*52);
         if(!cardsDelt.includes(newCard)){
-            let imageSrc = imageLink + cardImages[newCard];
-            image[cardsDelt.length].src=imageSrc;
             cardsDelt.push(newCard);
         }
     }
-    dealBtn.setAttribute('disabled', true);
-    drawBtn.removeAttribute('disabled');
+    hand = cardsDelt;
+    displayCards();
 }
 
 function drawCards() {
-    console.log(cardsDelt.length + cardsToHold.length);
-    dealBtn.removeAttribute('disabled');
-    drawBtn.setAttribute('disabled', true);
+    for(let i = 0; i < 5; i++){
+        if(cardsToHold.includes(image[i])) {
+            console.log(i);
+        }
+    }
+    /*while(cardsDelt.length < 5) {
+        let newCard = Math.floor(Math.random()*52);
+        if(!cardsDelt.includes(newCard)){
+
+        }
+    }*/
+}
+
+function displayCards() {
+    let i = 0;
+    hand.forEach(function(e){
+        let imageSrc = imageLink + cardImages[e];
+        image[i].src = imageSrc;
+        i++;
+    })
+}
+
+function scoreHand() {
 }
 
 function holdCard(e) {
     if(e.target.classList.contains('hold')){
         e.target.classList.remove('hold');
-        cardsToHold = arrayRemove(cardsToHold, e.target);
+        cardsToHold = removeHold(cardsToHold, e.target);
     } else {
         e.target.classList.add('hold');
         cardsToHold.push(e.target);
     }
-    console.log(e.target);
-    console.log(cardsToHold);
 }
 
-function arrayRemove(arr, value) {
+function removeHold(arr, value) {
     return arr.filter(function(ele){
         return ele != value;
     });
- 
  }
