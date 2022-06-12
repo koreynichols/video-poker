99f82e57ff22747a32f0cbd0bbd8fd29395ef406
Korey Nichols <koreynichols1@gmail.com>
2022-06-12 12:30:33 -0700

    fix: working on fixing problem with imageLink in js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const delay = 500;
-let imageLink = '/images/cardImages/';
+let imageLink = './images/cardImages/';
 let cardImages = ['AS.jpg', '2S.jpg', '3S.jpg', '4S.jpg', '5S.jpg', '6S.jpg', '7S.jpg', '8S.jpg', '9S.jpg', '10S.jpg', 'JS.jpg', 'QS.jpg', 'KS.jpg',
                   'AC.jpg', '2C.jpg', '3C.jpg', '4C.jpg', '5C.jpg', '6C.jpg', '7C.jpg', '8C.jpg', '9C.jpg', '10C.jpg', 'JC.jpg', 'QC.jpg', 'KC.jpg',
                   'AD.jpg', '2D.jpg', '3D.jpg', '4D.jpg', '5D.jpg', '6D.jpg', '7D.jpg', '8D.jpg', '9D.jpg', '10D.jpg', 'JD.jpg', 'QD.jpg', 'KD.jpg',
